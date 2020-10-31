7b14544ca059d87eb58439558f6028237b9e739c
Matt Olenik <olenikm@gmail.com>
2020-10-30 17:55:13 -0700

    Add TOML config

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/juju/errors v0.0.0-20200330140219-3fe23663418f
 	github.com/juju/testing v0.0.0-20201030020617-7189b3728523 // indirect
 	github.com/lixiangzhong/dnsutil v0.0.0-20191203032812-75ad39d2945a
+	github.com/pelletier/go-toml v1.8.1
 	github.com/rs/zerolog v1.20.0
 )
--- main.go
+++ main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/juju/errors"
 	"github.com/mattolenik/cloudflare-ddns-client/ip"
+	"github.com/pelletier/go-toml"
 	"github.com/rs/zerolog/log"
 )
 
 // version is populated by the ldflags argument during build.
 var version string
+
+const configFileName = "cloudflare-ddns.conf"
+
+// Config represents TOML program configuration
+type Config struct {
+	Domain string `toml:"domain"`
+	Record string `toml:"record"`
+}
 
 func main() {
 	err := mainE()
@@ -26,13 +36,20 @@ func mainE() error {
 	// Setting arg 0 makes sure that -help output has the correct program name when being invoked with "go run"
 	os.Args[0] = "cloudflare-ddns"
 	var flagVersion bool
+	var flagConfigPath string
 	flag.BoolVar(&flagVersion, "version", false, "Print the program version")
+	flag.StringVar(&flagConfigPath, "config", "/etc/"+configFileName, "Path to configuration file")
 
 	flag.Parse()
 
 	if flagVersion {
-		PrintVersion()
+		printVersion()
 	}
+	config, err := loadConfig(flagConfigPath)
+	if err != nil {
+		return errors.Trace(err)
+	}
+	fmt.Println(*config)
 
 	ip, err := ip.GetExternalIP()
 	if err != nil {
@@ -42,8 +59,22 @@ func mainE() error {
 	return nil
 }
 
-// PrintVersion prints the program version and exits.
-func PrintVersion() {
+// loadConfig loads the TOML configuration at the specified path.
+func loadConfig(path string) (*Config, error) {
+	config := &Config{}
+	contents, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, errors.Annotatef(err, "failed to read config file '%s", path)
+	}
+	err = toml.Unmarshal(contents, config)
+	if err != nil {
+		return nil, errors.Annotatef(err, "failed to parse config file '%s", path)
+	}
+	return config, nil
+}
+
+// printVersion prints the program version and exits.
+func printVersion() {
 	fmt.Println(version)
 	os.Exit(0)
 }
